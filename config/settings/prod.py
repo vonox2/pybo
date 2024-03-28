cceb5d992e67b92f98ab508a5da88a023b2f8bad
김동욱 <ehddnr2542@gmail.com>
2024-03-28 11:46:06 +0900

    update

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['52.79.104.162']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'otu4^Ceao]$WjT;hLUac$KMcughaK#:,',
+        'HOST': 'ls-d752fd884373e707741c861a374706734b11a371.cr0koe0mqklv.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
