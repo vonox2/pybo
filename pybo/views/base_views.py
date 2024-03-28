d2ba5d8414ee20c877c56313033581d28ba697c1
김동욱 <ehddnr2542@gmail.com>
2024-03-28 10:17:01 +0900

    update

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, Count
 
 from ..models import Question
+import logging
+logger = logging.getLogger('pybo')
 
 
 def index(request):
+    logger.info("INFO 레벨로 출력")
     """
     pybo 목록 출력
     """
